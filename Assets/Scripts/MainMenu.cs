434131c60ffebc3befde9fd90e51870810c0d92e
Ghassen-bgh <91068824+Ghassen-bgh@users.noreply.github.com>
2022-08-25 13:31:20 +0100

    colorblind mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,24 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     public void PlayMaze()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (colorblindMode.isOn)
+        {
+            Debug.Log("Colorblind mode is on");
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
+        
     }
 
     public void QuitMaze()
